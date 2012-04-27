f4fdfddf2dd478fb67f27722419a769d56aa7bae
Kilian Ciuffolo <kilian.ciuffolo@gmail.com>
2012-04-27 15:39:50 -0700

    added right alignment for callsite info
    moved options in a object hash
    added traceRight option
    added right alignment for callsite info

--- console-trace.js
+++ console-trace.js
@@ -6,6 +6,21 @@
 var callsite = require('callsite')
   , tty = require('tty')
 
+console.traceOptions = Object.create(null)
+
+/**
+ * Store custom options
+ *
+ * @param {Object} options
+ * @api public
+ */
+
+module.exports = function (options) {
+  if (opt) {
+    console.traceOptions = options;
+  }
+}
+
 /**
  * Overrides the console methods.
  */
@@ -13,8 +28,9 @@ var callsite = require('callsite')
 ;['error', 'log', 'info', 'warn'].forEach(function (name) {
   var fn = console[name];
   console[name] = function () {
-    if (console._trace || console.traceAlways) {
-      arguments[0] = console.traceFormat(__stack[1], name) + arguments[0];
+    if (console._trace || console.traceOptions.traceAlways) {
+      var pad = (arguments[0] && !console.traceOptions.traceRight ? ' ' : '');
+      arguments[0] =  console.traceFormat(__stack[1], name) + pad + arguments[0];
     }
     console._trace = false;
     return fn.apply(this, arguments);
@@ -31,10 +47,19 @@ var callsite = require('callsite')
 
 console.traceFormat = function (call, method) {
   var basename = call.getFileName().replace(process.cwd() + '/', '')
-    , str = '[' + basename + ':' + call.getLineNumber() + '] '
+    , str = '[' + basename + ':' + call.getLineNumber() + ']'
 
-  if (false === console.traceColors || tty.isatty()) {
-    return '\033[' + ('error' == method ? '91' : '90') + 'm' + str + '\033[39m';
+  if (false === console.traceOptions.traceColors || tty.isatty()) {
+    if (console.traceOptions.traceRight) {
+      var rowWidth = process.stdout.getWindowSize()[0];
+      return '\033[s' + // save current position
+             '\033[' + rowWidth + 'D' + // move to the start of the line
+             '\033[' + (rowWidth - str.length) + 'C' + // align right
+             '\033[' + ('error' == method ? '91' : '90') + 'm' + str + '\033[39m' +
+             '\033[u'; // restore current position
+    } else {
+      return '\033[' + ('error' == method ? '91' : '90') + 'm' + str + '\033[39m';
+    }
   } else {
     return str;
   }
--- test.js
+++ test.js
@@ -21,5 +21,33 @@ process.stdout.write(' ');
 
 console.traced.log('this is a traced console.log');
 
+// visual pad
+console.log('');
+
+console.log(' regular console.log, no clue where it came from');
+
+// visual pad
+process.stdout.write(' ');
+
+// change alignment
+console.traceOptions.traceRight = true;
+
+console.trace.log('this is a traced console.log');
+
+// visual pad
+console.log('');
+
+console.log(' regular console.log, no clue where it came from');
+
+// visual pad
+process.stdout.write(' ');
+
+// change alignment
+console.traceOptions.traceAlways = true;
+console.traceOptions.traceColors = false;
+
+console.error('this is a traced console.log');
+
+console.traceOptions.traceAlways = false;
 // pad
 console.log('');
